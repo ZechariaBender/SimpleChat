26f333f7e623aa85bb2849b40c29232e414c0930
ZechariaBender <zkbndr@gmail.com>
2019-07-28 10:31:09 +0300

    Initial commit

---
+++ src/main/java/com/ex4/demo/controllers/chatController.java
@@ -0,0 +1,22 @@
+package com.ex4.demo.controllers;
+
+import com.ex4.demo.repo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class chatController {
+
+    @GetMapping("/")
+    public String main(Model model, User user)
+    {
+        return "register";
+    }
+
+    @GetMapping("/chat")
+    public String chat(Model model, User user)
+    {
+        return "chat";
+    }
+}
